34af16843895d696a474311a4012d4c717d5e82f
rafika khatun <rafikakhatun607@gmail.com>
2025-05-04 20:13:16 +0530

    commit done

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors: {
+            PrimaryColor: "#FEA301",
+            CardGradient: "#DEF5FF to #FFFFFF",
+            AdditionalGradient: "#FFF6F6 to #EAC27B",
+            FooterColor: "#01ACFD",
+
+
+
+        },
+      },
+    },
+    plugins: [],
+  }
